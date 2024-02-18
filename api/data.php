a785beb4a52b8acaf602c475be8c78920ff4c98d
thisish <thisish@foxmail.com>
2024-02-18 22:36:10 +0800

    api test

---
+++ api/data.php
@@ -0,0 +1,32 @@
+<?php
+function httpRequest($url, $params, $post = true)
+{
+    $header = [
+            'Content-Type: application/json; charset=utf-8',
+    ];
+
+    $ch = curl_init();
+    if ($post) {
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+    } elseif (is_array($params) && 0 < count($params)) {
+        curl_setopt($ch, CURLOPT_URL, $url . "?" . http_build_query($params));
+    } else {
+        curl_setopt($ch, CURLOPT_URL, $url);
+    }
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FORBID_REUSE, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $data = curl_exec($ch);
+    if (curl_error($ch)) {
+        echo curl_error($ch);
+        return null;
+    }
+    curl_close($ch);
+    return $data;
+}
+
+echo httpRequest('https://tuanwei.nxu.edu.cn/info/1003/1022.htm', array(), false);
