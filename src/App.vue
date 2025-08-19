a6ca855c459181173bc0f6d6431b4bf6bcf25324
this-is-h <thisish@foxmail.com>
2025-08-19 18:48:33 +0800

    首次部署

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import docsearch from '@docsearch/js';
+
+import '@docsearch/css';
+
+window.onload = () => {
+    docsearch({
+        container: '#docsearch',
+        appId: 'U6YF89LHVL',
+        indexName: 'NDTW-TZGG',
+        apiKey: 'd660a32cc20d1728358c6df70dda8929',
+    })
+};
+</script>
+
+<template>
+  <div id="docsearch"></div>
+</template>
+
+<style scoped></style>
